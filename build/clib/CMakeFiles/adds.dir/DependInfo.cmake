9ddf0ea56e7157326cdb199a516b891cd9825ecc
Ayadh-sukri <ayahsukri11@gmail.com>
2023-12-11 16:37:16 +0530

    Added build and executables

---
+++ build/clib/CMakeFiles/adds.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ayadh/cpp/croot/clib/180_rotate.cpp" "clib/CMakeFiles/adds.dir/180_rotate.cpp.o" "gcc" "clib/CMakeFiles/adds.dir/180_rotate.cpp.o.d"
+  "/home/ayadh/cpp/croot/clib/90_rotate.cpp" "clib/CMakeFiles/adds.dir/90_rotate.cpp.o" "gcc" "clib/CMakeFiles/adds.dir/90_rotate.cpp.o.d"
+  "/home/ayadh/cpp/croot/clib/add.cpp" "clib/CMakeFiles/adds.dir/add.cpp.o" "gcc" "clib/CMakeFiles/adds.dir/add.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
